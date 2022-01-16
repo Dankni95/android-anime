39d6e5c8bc56eb4e70b02335a46ae1db4cbd3c4a
Daniel Lysak <Daniel_kn@live.no>
2022-01-16 22:08:11 +0100

    Added autocomplete and fetch (No button)

--- app/src/main/java/com/watch/anime/MainActivity.kt
+++ app/src/main/java/com/watch/anime/MainActivity.kt
@@ -7,7 +7,9 @@ import android.view.View
 import android.widget.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlin.collections.ArrayList
+import com.jakewharton.rxbinding.widget.RxTextView
+import rx.android.schedulers.AndroidSchedulers
+import java.util.concurrent.TimeUnit
 
 
 class MainActivity : Activity(), EpisodeAdapter.OnItemClickListener {
@@ -18,50 +20,43 @@ class MainActivity : Activity(), EpisodeAdapter.OnItemClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val autoCompleteTextView = findViewById<View>(R.id.autoTextView) as AutoCompleteTextView
-        val button = findViewById<Button>(R.id.btn)
         val epTextView = findViewById<View>(R.id.recycler_view) as RecyclerView
 
-
-        button.setOnClickListener {
-            val adapter: ArrayAdapter<String> = ArrayAdapter<String>(this, android.R.layout.select_dialog_item, searchedAnime)
-            val names = Query().execute(autoCompleteTextView.getText().toString())
-            val animes:Array<String> = names.get()
-            for (anime in animes){
-                searchedAnime.add(anime.replace("-", " ").replace("/category/",""))
+        RxTextView.textChanges(autoCompleteTextView)
+            .debounce(1, TimeUnit.SECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                searchedAnime.clear()
+                val adapter: ArrayAdapter<String> =
+                    ArrayAdapter<String>(this, android.R.layout.select_dialog_item, searchedAnime)
+                val names = Query().execute(autoCompleteTextView.editableText.toString())
+                val animes: Array<String> = names.get()
+                for (anime in animes) {
+                    searchedAnime.add(anime.replace("-", " ").replace("/category/", ""))
+                }
+                autoCompleteTextView.setAdapter(adapter)
+                autoCompleteTextView.postDelayed({
+                    autoCompleteTextView.showDropDown()
+                }, 10)
             }
 
-            autoCompleteTextView.setAdapter(adapter)
-            autoCompleteTextView.postDelayed({
-                autoCompleteTextView.showDropDown()
-            }, 10)
-
             autoCompleteTextView.setOnItemClickListener { parent, arg1, pos, id ->
-                Toast.makeText(
-                    this,
-                    autoCompleteTextView.editableText.toString(),
-                    Toast.LENGTH_LONG
-                ).show()
-
-
-
                 val episode = Episode()
                 val exampleList = generateDummyList(episode.execute(autoCompleteTextView.editableText.toString()).get().toInt(),autoCompleteTextView)
-
                 val rAdapter = EpisodeAdapter(exampleList)
 
                 epTextView.adapter = rAdapter
                 epTextView.layoutManager = LinearLayoutManager(this)
                 epTextView.setHasFixedSize(true)
-                }
+                autoCompleteTextView.editableText.clear()
             }
-
         }
 
     private fun generateDummyList(size: Int, autoCompleteTextView:AutoCompleteTextView): ArrayList<Items> {
 
         val list = ArrayList<Items>()
 
-        for (i in 1 until size) {
+        for (i in 1 until size +1) {
             val item = Items(R.drawable.ic_android_black_24dp, "Episode $i", autoCompleteTextView.editableText.toString())
             list += item
         }
@@ -69,7 +64,7 @@ class MainActivity : Activity(), EpisodeAdapter.OnItemClickListener {
         return list
     }
 
-    // TODO: Onlick does not work ://
+    // TODO: Onclick does not work ://
 
     override fun onItemClick(position: Int, exampleList:List<Items>, rAdapter:EpisodeAdapter) {
         Toast.makeText(this, "Item $position clicked", Toast.LENGTH_SHORT).show()
