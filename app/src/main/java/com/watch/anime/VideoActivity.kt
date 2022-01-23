53875581663f92eeb5c44e42328f8826b36b8e97
Daniel Lysak <Daniel_kn@live.no>
2022-01-23 01:52:01 +0100

    Change mediaplayer to ExoPlayer

--- app/src/main/java/com/watch/anime/VideoActivity.kt
+++ app/src/main/java/com/watch/anime/VideoActivity.kt
@@ -1,122 +1,55 @@
 package com.watch.anime
 
 import android.app.Activity
-import android.media.MediaPlayer
-import android.net.Uri
 import android.os.Bundle
-import android.util.DisplayMetrics
 import android.view.*
-import android.widget.Button
-import android.widget.ProgressBar
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.ui.PlayerView
 import java.io.IOException
-import java.util.concurrent.TimeUnit
 
 
 class VideoActivity : Activity() {
-    private var surfaceView: SurfaceView? = null
-    private var player: MediaPlayer? = Play().execute("").get()
-    private var progressBar: ProgressBar? = null
-    var buttonPlay: Button? = null
+    private var surfaceView: PlayerView? = null
+    private var player: ExoPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.videoview)
-        surfaceView = findViewById<View>(R.id.surfaceView) as SurfaceView
-        progressBar = findViewById<View>(R.id.progress) as ProgressBar
-        buttonPlay = findViewById<View>(R.id.buttonPlay) as Button
+        surfaceView = findViewById<View>(R.id.surfaceView) as PlayerView
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+
+        player = ExoPlayer.Builder(surfaceView!!.context).build()
+        surfaceView!!.setPlayer(player)
 
 
         val extras = intent.extras
         val url = extras?.getString("video_link");
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        surfaceView!!.setZOrderOnTop(true)
 
-        // TODO Read up on services, this needs to be moved
+        val window: Window = getWindow()
 
-        val displayMetrics = DisplayMetrics()
-        windowManager.defaultDisplay.getMetrics(displayMetrics)
-
-        val videoParams: ViewGroup.LayoutParams = surfaceView!!.getLayoutParams()
-        videoParams.width = displayMetrics.widthPixels
-        videoParams.height = displayMetrics.heightPixels
-
-        hideSystemBars(true)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.setNavigationBarColor(getResources().getColor(R.color.black));
 
 
         try {
-            player!!.setDataSource(this, Uri.parse(url))
-            surfaceView!!.holder.addCallback(MyCallBack())
-            player!!.prepare()
-            player!!.setOnPreparedListener {
-                progressBar!!.visibility = View.INVISIBLE
-                player!!.setVolume(100F, 100F);
-                player!!.start()
-                player!!.isLooping = true
 
+            val mediaItem: MediaItem? = url?.let { MediaItem.fromUri(it) }
+
+            if (mediaItem != null) {
+                player!!.setMediaItem(mediaItem)
             }
+            player!!.prepare()
+            player!!.play()
+
         } catch (e: IOException) {
             e.printStackTrace()
         }
-
-        (surfaceView)!!.setOnClickListener {
-
-            //TODO button is not visible :(
-            buttonPlay!!.visibility = View.VISIBLE
-            buttonPlay?.bringToFront()
-
-            hideSystemBars(false)
-            TimeUnit.SECONDS.sleep(3);
-            hideSystemBars(true)
-
-
-        }
-
-        buttonPlay!!.setOnClickListener{
-            playPause()
-        }
-
-
-    }
-    fun playPause() {
-        if (player?.isPlaying == true) {
-            player?.start()
-            buttonPlay?.setText("Pause")
-        } else {
-            player?.pause()
-            buttonPlay?.setText("Play")
-        }
     }
 
-    fun hideSystemBars(toHide:Boolean) {
-        if (toHide){
-            val windowInsetsController =
-                ViewCompat.getWindowInsetsController(window.decorView) ?: return
-            // Configure the behavior of the hidden system bars
-            windowInsetsController.systemBarsBehavior =
-                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            // Hide both the status bar and the navigation bar
-            windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
-        }else{
-            val windowInsetsController =
-                ViewCompat.getWindowInsetsController(window.decorView) ?: return
-            // Configure the behavior of the hidden system bars
-            windowInsetsController.systemBarsBehavior =
-                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            // Hide both the status bar and the navigation bar
-            windowInsetsController.show(WindowInsetsCompat.Type.systemBars())
-        }
-    }
 
-    private inner class MyCallBack : SurfaceHolder.Callback {
-        override fun surfaceCreated(holder: SurfaceHolder) {
-            player!!.setDisplay(holder)
-        }
-
-        override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {}
-        override fun surfaceDestroyed(holder: SurfaceHolder) {}
-    }
 
     override fun onStop() {
         super.onStop()
@@ -136,7 +69,7 @@ class VideoActivity : Activity() {
     override fun onResume() {
         super.onResume()
         if (!player?.isPlaying()!!) {
-            player!!.start()
+            player!!.play()
         }
     }
 }
